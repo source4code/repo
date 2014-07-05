bc4f8f13fbd9e042a5194689da8976aa11e9b5d0
source4code.info <source4code.info@gmail.com>
2014-07-05 08:33:10 +0200

    initial commit

---
+++ jaxb-unmarshal-string/src/main/java/info/source4code/jaxb/model/Car.java
@@ -0,0 +1,45 @@
+package info.source4code.jaxb.model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(namespace = "info.source4code.jaxb.model", name = "Car")
+public class Car {
+
+    private String make;
+    private String manufacturer;
+    private String id;
+
+    public String getMake() {
+        return make;
+    }
+
+    @XmlElement
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @XmlElement
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @XmlAttribute
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return "Car [" + "make=" + make + ", manufacturer=" + manufacturer
+                + ", id=" + id + "]";
+    }
+}
