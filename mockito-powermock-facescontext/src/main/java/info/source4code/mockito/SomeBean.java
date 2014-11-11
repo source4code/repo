e8bd4799d9b33cdf3e0353ef3cd8ac27b1c066ae
source4code.info <source4code.info@gmail.com>
2014-11-11 19:58:59 +0100

    added @SessionScoped

--- mockito-powermock-facescontext/src/main/java/info/source4code/mockito/SomeBean.java
+++ mockito-powermock-facescontext/src/main/java/info/source4code/mockito/SomeBean.java
@@ -3,9 +3,11 @@ package info.source4code.mockito;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
+@SessionScoped
 public class SomeBean {
 
     public void addMessage(Severity severity, String summary, String detail) {
