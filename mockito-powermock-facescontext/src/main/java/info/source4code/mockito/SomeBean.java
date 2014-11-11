8a12733126cd304b7280ed8fba9f96c38de1394a
source4code.info <source4code.info@gmail.com>
2014-11-11 08:24:06 +0100

    initial commit

---
+++ mockito-powermock-facescontext/src/main/java/info/source4code/mockito/SomeBean.java
@@ -0,0 +1,22 @@
+package info.source4code.mockito;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class SomeBean {
+
+    public void addMessage(Severity severity, String summary, String detail) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(severity, summary, detail));
+    }
+
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext()
+                .invalidateSession();
+
+        return "logout?faces-redirect=true";
+    }
+}
