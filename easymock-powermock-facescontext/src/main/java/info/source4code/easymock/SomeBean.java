907d918fa09538c530dfbb15ec091d7185b14d89
source4code.info <source4code.info@gmail.com>
2014-11-12 21:46:18 +0100

    initial commit

---
+++ easymock-powermock-facescontext/src/main/java/info/source4code/easymock/SomeBean.java
@@ -0,0 +1,24 @@
+package info.source4code.easymock;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class SomeBean {
+
+    public void addMessage(Severity severity, String summary, String detail) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(severity, summary, detail));
+    }
+
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext()
+                .invalidateSession();
+
+        return "logout?faces-redirect=true";
+    }
+}
